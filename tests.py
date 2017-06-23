679c0a62e2ace867a1a4d36e76af2dc3ba7b7bec
Simon Egersand <s.egersand@gmail.com>
2017-06-24 01:06:28 +0200

    Add more complex test cases for parser and tokenizer

--- tests.py
+++ tests.py
@@ -1,16 +1,27 @@
+#!/usr/bin/env python3
+
 import unittest
 from tokenizer import *
+from parser import *
 
 class TestTokenize(unittest.TestCase):
 
-    def test_numbers(self):
-        # Valid number literals
+    def test_simple_numbers(self):
         self.assertEqual(tokenize('3'), [('3')])
         self.assertEqual(tokenize('+3'), [('+3')])
         self.assertEqual(tokenize('-4'), [('-4')])
-        # Not valid (considered symbols)
         self.assertEqual(tokenize('3.5'), ['3.5'])
         self.assertEqual(tokenize('-2-4'), ['-2-4'])
+
+    def test_nested(self):
+        self.assertEqual(tokenize("(begin (define r 10) (* pi (* r r)))"), ['(', 'begin', '(', 'define', 'r', '10', ')', '(', '*', 'pi', '(', '*', 'r', 'r', ')', ')', ')'])
+
+
+class TestParser(unittest.TestCase):
+
+    def test(self):
+        self.assertEqual(parse("(begin (define r 10) (* pi (* r r)))"), ['begin', ['define', 'r', 10], ['*', 'pi', ['*', 'r', 'r']]])
+
 
 if __name__ == '__main__':
     unittest.main()
