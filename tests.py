aa212d89fd8915c0cdc47e8db17001a4cb20300f
Simon Egersand <s.egersand@gmail.com>
2017-06-23 23:08:41 +0200

    Make tests runnable

--- tests.py
+++ tests.py
@@ -5,10 +5,12 @@ class TestTokenize(unittest.TestCase):
 
     def test_numbers(self):
         # Valid number literals
-        self.assertEqual(tokenize("3"), [('literal', 3)])
-        self.assertEqual(tokenize("+3"), [('literal', 3)])
-        self.assertEqual(tokenize("-4"), [('literal', -4)])
+        self.assertEqual(tokenize('3'), [('3')])
+        self.assertEqual(tokenize('+3'), [('+3')])
+        self.assertEqual(tokenize('-4'), [('-4')])
         # Not valid (considered symbols)
-        self.assertEqual(tokenize("3.5"), [('symbol', '2.5')])
-        self.assertEqual(tokenize("-2-4"), [('symbol', '-2-4')])
+        self.assertEqual(tokenize('3.5'), ['3.5'])
+        self.assertEqual(tokenize('-2-4'), ['-2-4'])
 
+if __name__ == '__main__':
+    unittest.main()
