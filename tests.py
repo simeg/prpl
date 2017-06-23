69630dca582d9ee197a886bd9fd380df4102bcaa
Simon Egersand <s.egersand@gmail.com>
2017-06-24 01:31:04 +0200

    Test parser.create_ast

--- tests.py
+++ tests.py
@@ -13,14 +13,19 @@ class TestTokenize(unittest.TestCase):
         self.assertEqual(tokenize('3.5'), ['3.5'])
         self.assertEqual(tokenize('-2-4'), ['-2-4'])
 
-    def test_nested(self):
+    def test_article_example(self):
         self.assertEqual(tokenize("(begin (define r 10) (* pi (* r r)))"), ['(', 'begin', '(', 'define', 'r', '10', ')', '(', '*', 'pi', '(', '*', 'r', 'r', ')', ')', ')'])
 
 
 class TestParser(unittest.TestCase):
 
-    def test(self):
+    def test_parse_article_example(self):
         self.assertEqual(parse("(begin (define r 10) (* pi (* r r)))"), ['begin', ['define', 'r', 10], ['*', 'pi', ['*', 'r', 'r']]])
+
+    def test_create_ast(self):
+        self.assertEqual(create_ast(['(', '+', '2', '3', ')']), ['+', 2, 3])
+        self.assertEqual(create_ast(['(', '(', '+', '2', '3', ')', '(', '*', '3', '2', ')', ')']), [['+', 2, 3], ['*', 3, 2]])
+        self.assertEqual(create_ast(['(', '(', '+', '2', '3', ')', '(', '*', 'pi', '(', '*', '2', '2', ')', ')', ')']), [['+', 2, 3], ['*', 'pi', ['*', 2, 2]]])
 
 
 if __name__ == '__main__':
