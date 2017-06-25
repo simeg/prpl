991ed072e6feb6a27639493e87351a0aefc6a467
Simon Egersand <s.egersand@gmail.com>
2017-06-25 20:49:53 +0200

    Output error type on unknown error

--- errors.py
+++ errors.py
@@ -4,17 +4,22 @@ class SyntaxException(Exception):
     pass
 
 
+class ElementNotFoundException(Exception):
+    pass
+
+
 class UnknownException(Exception):
     pass
 
 exception_types = {
-        'syntax': SyntaxException
+        'syntax': SyntaxException,
+        'element_not_found': ElementNotFoundException,
         }
 
 
-def throw_error(err_type, msg):
+def throw_error(err_type, msg=''):
     if err_type in exception_types:
         raise exception_types[err_type](msg)
     else:
-        raise UnknownException('Unknown exception type')
+        raise UnknownException('Unknown exception type: %s' % err_type)
 
